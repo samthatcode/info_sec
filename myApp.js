d9e7ff72ccfbb224e2411102b79eb9271b87c8d2
samthatcode <samthatcode@gmail.com>
2023-11-04 03:29:20 +0100

    info_sec server queries for attacks

--- myApp.js
+++ myApp.js
@@ -1,48 +1,35 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
+
+// Configure helmet middleware for HSTS with HTTPS for the next 90 days
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Configure helmet middleware with a configuration object
+app.use(
+  helmet({
+    hidePoweredBy: {}, // Remove X-Powered-By header
+    frameguard: {
+      action: 'DENY',
+    },
+    xssFilter: {}, // Sanitize input to your server
+    noSniff: {}, // Set X-Content-Type-Options header to nosniff
+    ieNoOpen: {}, // Set X-Download-Options header to noopen
+    hsts: {
+      maxAge: ninetyDaysInSeconds,
+      force: true,
+    }, // Configure HSTS with HTTPS for the next 90 days
+    dnsPrefetchControl: {}, // Enable DNS prefetch control
+    noCache: {}, // Enable noCache
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", 'trusted-cdn.com'],
+      },
+    }, // Configure Content Security Policy (CSP)
+  })
+);
 
 
 
